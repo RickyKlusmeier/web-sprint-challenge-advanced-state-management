2aadb8f1eddf82771f9ee09e56581cd7e962ee2e
Ricky Klusmeier <rickyklusmeier@Rickys-MacBook-Pro-2.local>
2021-04-16 11:43:45 -0600

    MVP

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux'
 import "./index.css";
 import App from "./App";
-import { reducer } from './reducers'
+import reducer from './reducers'
 
 
 const { worker } = require('./mocks/browser');
